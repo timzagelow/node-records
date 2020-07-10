599a9b6b3a75bc5f7881945626425f45776e7160
timzagelow <timzagelow@gmail.com>
2020-07-10 13:53:16 -0700

    better error handling

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
+const handleErrors = require('./middleware/handleErrors');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -21,5 +22,6 @@ const routes = require('./routes');
 
 app.use('/records', routes);
 
+app.use(handleErrors);
 
 app.listen(port, () => console.log('Up!'));
--- routes.js
+++ routes.js
@@ -1,56 +1,52 @@
 const routes = require('express').Router();
 let db = require('./db');
 let aliasHelper = require('./helpers/alias');
+const { BadRequest, GeneralError } = require('./helpers/errors');
 
 routes.put('/:id', (req, res, next) => {
     let updates = {};
     let item = req.body;
 
-    if (!item) {
-        return res.status(400).json({
-            status: 'error',
-            message: 'No fields to update'
-        });
-    }
-
-    for (let alias in item) {
-        let key = aliasHelper.findKeyByAlias(alias);
-
-        if (key) {
-            updates[key] = item[alias];
+    try {
+        if (!item) {
+            throw new BadRequest('No fields to update');
         }
-    }
 
-    if (!updates.length) {
-        return res.status(400).json({
-            status: 'error',
-            message: 'No fields to update'
-        });
-    }
+        for (let alias in item) {
+            let key = aliasHelper.findKeyByAlias(alias);
 
-    let update = `UPDATE ${process.env.MYSQL_RECORDS_TABLE} SET `;
-    let values = [];
-
-    for (let key in updates) {
-        update += `\`${key}\` = ?, `;
-        values.push(updates[key]);
-    }
-
-    update = update.slice(0, -2);
-    update += ` WHERE InventoryNo = ${req.params.id}`;
-
-    db.then((connection => {
-        connection.execute(update, values, (err) => {
-            if (err) {
-                return res.status(500).json({
-                    status: 'error',
-                    message: err.message
-                });
+            if (key) {
+                updates[key] = item[alias];
             }
+        }
 
-            res.sendStatus(200);
-        });
-    }));
+        if (!updates.length) {
+            throw new BadRequest('No fields to update');
+        }
+
+        let update = `UPDATE ${process.env.MYSQL_RECORDS_TABLE} SET `;
+        let values = [];
+
+        for (let key in updates) {
+            update += `\`${key}\` = ?, `;
+            values.push(updates[key]);
+        }
+
+        update = update.slice(0, -2);
+        update += ` WHERE InventoryNo = ${req.params.id}`;
+
+        db.then((connection => {
+            connection.execute(update, values, (err) => {
+                if (err) {
+                    throw new GeneralError(err.message);
+                }
+
+                res.sendStatus(200);
+            });
+        }));
+    } catch (err) {
+        next(err);
+    }
 });
 
 routes.get('/', (req, res) => {
